490a58498fa8e5e6bd8f01596317e3b56cef3bd2
Brandon Newman <bnewman@umail.ucsb.edu>
2013-05-13 20:18:03 -0700

    Attempted to build build.xml

---
+++ src/edu/ucsb/cs56/projects/games/pacman/PacMan.java
@@ -0,0 +1,23 @@
+package edu.ucsb.cs56.projects.games.pacman;
+
+import javax.swing.JFrame;
+import edu.ucsb.cs56.projects.games.pacman.Board;
+
+
+public class PacMan extends JFrame
+{
+
+    public PacMan()
+    {
+		add(new Board());
+		setTitle("Pacman");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(380, 420);
+		setLocationRelativeTo(null);
+		setVisible(true);
+    }
+
+    public static void main(String[] args){
+		new PacMan();
+    }
+}
