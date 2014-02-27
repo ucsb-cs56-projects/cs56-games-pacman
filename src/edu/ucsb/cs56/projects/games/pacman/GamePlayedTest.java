3444fcd6385ba8b636784cef5c34e6085dd9d443
Katya <kfomenko@umail.ucsb.edu>
2014-02-27 00:50:17 -0800

    Uncommented out the two JUnit Test files

--- src/edu/ucsb/cs56/projects/games/pacman/GamePlayedTest.java
+++ src/edu/ucsb/cs56/projects/games/pacman/GamePlayedTest.java
@@ -1,48 +1,48 @@
-//~ package edu.ucsb.cs56.projects.games.pacman;
-//~ 
-//~ import static org.junit.Test;
-//~ import static org.junit.Assert.assertEquals;
-//~ import static org.junit.Assert.assertTrue;
-//~ import static org.junit.Assert.assertFalse;
-//~ import java.util.Date;
-//~ 
-//~ /** Test class for GamePlayed
-	//~ @author Kateryna Fomenko
-	//~ @author Deanna Hartsook
-	//~ @version CS56, Winter 2014
-	//~ @see GamePlayed
-//~ */
-//~ 
-//~ public class GamePlayedTest{
-	//~ /** Test case for getName() method of GamePlayed
-	 //~ * @see GamePlayed#getName
-	 //~ */
-	 //~ @Test
-	 //~ public void test_getName(){
-		//~ Date d = new Date();
-		//~ GamePlayed g1 = new GamePlayed("Barbara", d, 2000);
-		//~ assertEquals("Barbara", g1.getName());
-	 //~ }
-	 //~ 
-	 //~ /** Test case for getDate() method of GamePlayed
-	 //~ * @see GamePlayed#getDate
-	 //~ */
-	 //~ @Test
-	 //~ public void test_getDate(){
-		//~ Date d = new Date();
-		//~ GamePlayed g1 = new GamePlayed("Barbara", d, 2000);
-		//~ assertEquals(d, g1.getDate());
-	 //~ }
-	 //~ /** Test case for getScore() method of GamePlayed
-	 //~ * @see GamePlayed#getScore
-	 //~ */
-	 //~ @Test
-	 //~ public void test_getScore(){
-		//~ Date d = new Date();
-		//~ GamePlayed g1 = new GamePlayed("Barbara", d, 2000);
-		//~ assertEquals(2000, g1.getScore());
-	 //~ }
-	 //~ 
-	 //~ 
-//~ }
-//~ 
+package edu.ucsb.cs56.projects.games.pacman;
+
+import static org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import java.util.Date;
+
+/** Test class for GamePlayed
+	@author Kateryna Fomenko
+	@author Deanna Hartsook
+	@version CS56, Winter 2014
+	@see GamePlayed
+*/
+
+public class GamePlayedTest{
+	/** Test case for getName() method of GamePlayed
+	 * @see GamePlayed#getName
+	 */
+	 @Test
+	 public void test_getName(){
+		Date d = new Date();
+		GamePlayed g1 = new GamePlayed("Barbara", d, 2000);
+		assertEquals("Barbara", g1.getName());
+	 }
+	 
+	 /** Test case for getDate() method of GamePlayed
+	 * @see GamePlayed#getDate
+	 */
+	 @Test
+	 public void test_getDate(){
+		Date d = new Date();
+		GamePlayed g1 = new GamePlayed("Barbara", d, 2000);
+		assertEquals(d, g1.getDate());
+	 }
+	 /** Test case for getScore() method of GamePlayed
+	 * @see GamePlayed#getScore
+	 */
+	 @Test
+	 public void test_getScore(){
+		Date d = new Date();
+		GamePlayed g1 = new GamePlayed("Barbara", d, 2000);
+		assertEquals(2000, g1.getScore());
+	 }
+	 
+	 
+}
+
