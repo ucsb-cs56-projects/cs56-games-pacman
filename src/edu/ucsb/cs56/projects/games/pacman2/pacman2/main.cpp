cd9d2751b4fe27dfc13bc38fa494296aee0dbf8b
Deanna Hartsook <dmhartsook@gmail.com>
2014-02-26 21:12:56 -0800

    Initial Commit

---
+++ src/edu/ucsb/cs56/projects/games/pacman2/pacman2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pacman2
+//
+//  Created by Deanna Hartsook on 2/26/14.
+//  Copyright (c) 2014 Deanna Hartsook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
