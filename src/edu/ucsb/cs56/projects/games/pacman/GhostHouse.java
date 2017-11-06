4929ad6832602d0029b684dd00958dcb911f0ae3
Nicholas Duncan <ncduncan@umail.ucsb.edu>
2017-11-05 16:33:47 -0800

    updated documentation on Ghost House class

--- src/edu/ucsb/cs56/projects/games/pacman/GhostHouse.java
+++ src/edu/ucsb/cs56/projects/games/pacman/GhostHouse.java
@@ -14,15 +14,21 @@ import java.util.LinkedList;
 */
 
 public class GhostHouse{
-    private Queue<Ghost> ghosts;
+    private Queue<Ghost> ghosts; //ghosts held in the ghosthouse
     private Location topLeft; //location of the top left block of the GhostHouse
     private int width; //amount of blocks horizontally contained in the ghostHouse
-    private final static double releaseTime = 3;
-    private long lastReleaseTime = 0;
-    private boolean timerRunning = false;
-    private int blockSize;
+    private final static double releaseTime = 3; //time between releases of ghosts
+    private long lastReleaseTime = 0; //time that a ghost was last released
+    private boolean timerRunning = false; //not currently used but could be implemented to turn timer on/off
+    private int blockSize; //size in pixels of a block on the map (from board class)
 
-    
+    /**
+     *Constructor for a ghosthouse object
+     *note that location must matched with the level map data that is created
+     *@param topLeft Location of the top left corner of the ghost house
+     *@param width how many "blocks" the house encompasses (typically 1 block per ghost)
+     *@param blockSize the size of each block as defined by the board class (must pass this in)
+     */
     public GhostHouse(Location topLeft, int width, int blockSize){
 	ghosts = new LinkedList<Ghost>();
 	this.topLeft = topLeft;
@@ -33,31 +39,50 @@ public class GhostHouse{
 	System.out.println("    TopLeft: " + topLeft.getX() + ", " + topLeft.getY());
     }
 
+    /**
+     *Method to add a ghost to a ghost house object
+     *Ghosts in ghost house will properly spawn and respawn
+     *@param ghost the ghost desired to be put into the ghost house
+     */
     public void addGhost(Ghost ghost){
 	ghosts.add(ghost);
-	ghost.setSpeed(0);
+	ghost.setSpeed(0); //ghost speed is now set to 0 so that ghosts won't run through ghost house walls
     }
 
+    /**
+     *returns the top left location of the ghost house
+     */
     public Location getTopLeft(){
 	return topLeft;
     }
 
+    /**
+     *returns the width of the ghost house
+     */
     public int getWidth(){
 	return width;
     }
 
+    /**
+     *This method updates the timer on the ghost house so that ghosts
+     *are released at proper intervals - call this in the logic updating loops
+     */
     public void update(){
 	long currentTime = System.currentTimeMillis() / 1000; //in seconds
 	if(ghosts.size() == 0){
 	    lastReleaseTime = currentTime;
 	    return;
 	}
-	if(currentTime - lastReleaseTime > releaseTime){
-	    releaseGhost();
-	    lastReleaseTime = currentTime;
+	if(currentTime - lastReleaseTime > releaseTime){ //if its been over (releaseTime) second(s) since a ghost has last been released
+	    releaseGhost(); 
+	    lastReleaseTime = currentTime; //another ghost will be released in (releaseTime) second(s)
 	}
     }
 
+    /**
+     *Takes a ghost out of the ghost house and spawns it at the location 
+     *above the ghost house
+     */
     private void releaseGhost(){
 	if(ghosts.size() <= 0){
 	    return;
@@ -67,11 +92,14 @@ public class GhostHouse{
 	ghost.setX(releaseLocation.getX() * blockSize); //setting ghost x in pixels
 	ghost.setY(releaseLocation.getY() * blockSize); //setting ghost y in pixels
 
-	ghost.setSpeed((int) (Math.random() * Ghost.defaultSpeed) + 1);
+	ghost.setSpeed((int) (Math.random() * Ghost.defaultSpeed) + 1); //ghost speed is randomly set upon release
 	
 	System.out.println("Spawning Point: " + releaseLocation.getX() + ", " + releaseLocation.getY());
     }
-    
+    /**
+     *Resets the timer on the ghost house
+     *must wait a full time interval before the next ghost is released
+     */
     public void resetTimer(){
 	lastReleaseTime = 0;
     }
