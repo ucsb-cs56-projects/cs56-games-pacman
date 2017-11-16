c966f1393a271fe1138bbe50bdbde8e05533e361
Wei Tung Chen <lleeoochen@gmail.com>
2017-11-15 17:30:58 -0800

    Finished GhostHouse JUnit Test

--- src/edu/ucsb/cs56/projects/games/pacman/GhostHouseTest.java
+++ src/edu/ucsb/cs56/projects/games/pacman/GhostHouseTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs56.projects.games.pacman;
 
 import org.junit.Test;
+import java.util.List;
+import java.util.LinkedList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,11 +14,10 @@ import static org.junit.Assert.assertEquals;
  * @version CS56 F17
  * @see GhostHouse
  */
-
 public class GhostHouseTest {
 	
 	@Test
-	public void test_constructor_simple() {
+	public void test_constructor() {
 		GhostHouse ghostHouse = new GhostHouse(new Location(0, 1), 3, 20);
 		assertEquals(0, ghostHouse.getTopLeft().getX());
 		assertEquals(1, ghostHouse.getTopLeft().getY());
@@ -27,7 +28,31 @@ public class GhostHouseTest {
 	public void test_addGhost() {
 		GhostHouse ghostHouse = new GhostHouse(new Location(0, 1), 3, 20);
 		ghostHouse.addGhost(new Ghost(0, 0, 3, Ghost.GHOST1));
-		//assertEquals(1, ghostHouse.getGhosts().size());
+		assertEquals(1, ghostHouse.getGhosts().size());
 	}
 
+	@Test
+	public void test_addGhosts() {
+		List<Ghost> ghosts = new LinkedList<Ghost>();
+		ghosts.add(new Ghost(0, 0, 3, Ghost.GHOST1));
+		ghosts.add(new Ghost(0, 0, 3, Ghost.GHOST1));
+		ghosts.add(new Ghost(0, 0, 3, Ghost.GHOST1));
+
+		GhostHouse ghostHouse = new GhostHouse(new Location(0, 1), 3, 20);
+		ghostHouse.addGhosts(ghosts);
+		assertEquals(3, ghostHouse.getGhosts().size());
+	}
+
+	@Test
+	public void test_update() {
+		List<Ghost> ghosts = new LinkedList<Ghost>();
+		ghosts.add(new Ghost(0, 0, 3, Ghost.GHOST1));
+		ghosts.add(new Ghost(0, 0, 3, Ghost.GHOST1));
+		ghosts.add(new Ghost(0, 0, 3, Ghost.GHOST1));
+
+		GhostHouse ghostHouse = new GhostHouse(new Location(0, 1), 3, 20);
+		ghostHouse.addGhosts(ghosts);
+		ghostHouse.update(); //should release one ghost
+		assertEquals(2, ghostHouse.getGhosts().size());
+	}
 }
