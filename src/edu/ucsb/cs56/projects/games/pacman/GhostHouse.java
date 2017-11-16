7d322906d5abd79250831475bc4c6e8cdf5ed16a
Wei Tung Chen <lleeoochen@gmail.com>
2017-11-15 17:30:16 -0800

    Added ghost getters and fixed indentation in GhostHouse.java

--- src/edu/ucsb/cs56/projects/games/pacman/GhostHouse.java
+++ src/edu/ucsb/cs56/projects/games/pacman/GhostHouse.java
@@ -31,13 +31,13 @@ public class GhostHouse{
      *@param blockSize the size of each block as defined by the board class (must pass this in)
      */
     public GhostHouse(Location topLeft, int width, int blockSize){
-	ghosts = new LinkedList<Ghost>();
-	this.topLeft = topLeft;
-	this.width = width;
-	this.blockSize = blockSize;
+        ghosts = new LinkedList<Ghost>();
+        this.topLeft = topLeft;
+        this.width = width;
+        this.blockSize = blockSize;
 
-	System.out.println("Intialize ---------");
-	System.out.println("    TopLeft: " + topLeft.getX() + ", " + topLeft.getY());
+        //System.out.println("Intialize ---------");
+        //System.out.println("    TopLeft: " + topLeft.getX() + ", " + topLeft.getY());
     }
 
     /**
@@ -46,8 +46,8 @@ public class GhostHouse{
      *@param ghost the ghost desired to be put into the ghost house
      */
     public void addGhost(Ghost ghost){
-	ghosts.add(ghost);
-	ghost.setSpeed(0); //ghost speed is now set to 0 so that ghosts won't run through ghost house walls
+        ghosts.add(ghost);
+        ghost.setSpeed(0); //ghost speed is now set to 0 so that ghosts won't run through ghost house walls
     }
 
     /**
@@ -58,7 +58,7 @@ public class GhostHouse{
     public void addGhosts(List<Ghost> ghosts){
         //Empty ghost house
         while (this.ghosts.size() != 0)
-            this.ghosts.remove();
+        this.ghosts.remove();
 
         //Add ghosts to ghost house
         for (Ghost ghost1 : ghosts) {
@@ -71,14 +71,21 @@ public class GhostHouse{
      *returns the top left location of the ghost house
      */
     public Location getTopLeft(){
-	return topLeft;
+        return topLeft;
     }
 
     /**
      *returns the width of the ghost house
      */
     public int getWidth(){
-	return width;
+        return width;
+    }
+
+    /**
+     *returns the ghosts list
+     */
+    public Queue<Ghost> getGhosts(){
+        return ghosts;
     }
 
     /**
@@ -86,15 +93,15 @@ public class GhostHouse{
      *are released at proper intervals - call this in the logic updating loops
      */
     public void update(){
-	long currentTime = System.currentTimeMillis() / 1000; //in seconds
-	if(ghosts.size() == 0){
-	    lastReleaseTime = currentTime;
-	    return;
-	}
-	if(currentTime - lastReleaseTime > releaseTime){ //if its been over (releaseTime) second(s) since a ghost has last been released
-	    releaseGhost(); 
-	    lastReleaseTime = currentTime; //another ghost will be released in (releaseTime) second(s)
-	}
+        long currentTime = System.currentTimeMillis() / 1000; //in seconds
+        if(ghosts.size() == 0){
+            lastReleaseTime = currentTime;
+            return;
+        }
+        if(currentTime - lastReleaseTime > releaseTime){ //if its been over (releaseTime) second(s) since a ghost has last been released
+            releaseGhost(); 
+            lastReleaseTime = currentTime; //another ghost will be released in (releaseTime) second(s)
+        }
     }
 
     /**
@@ -102,23 +109,23 @@ public class GhostHouse{
      *above the ghost house
      */
     private void releaseGhost(){
-	if(ghosts.size() <= 0){
-	    return;
-	}
-	Ghost ghost = ghosts.remove();
-	Location releaseLocation = new Location(topLeft.getX() + ((this.width / 2) + 1), topLeft.getY() - 1);
-	ghost.setX(releaseLocation.getX() * blockSize); //setting ghost x in pixels
-	ghost.setY(releaseLocation.getY() * blockSize); //setting ghost y in pixels
+        if(ghosts.size() <= 0){
+            return;
+        }
+        Ghost ghost = ghosts.remove();
+        Location releaseLocation = new Location(topLeft.getX() + ((this.width / 2) + 1), topLeft.getY() - 1);
+        ghost.setX(releaseLocation.getX() * blockSize); //setting ghost x in pixels
+        ghost.setY(releaseLocation.getY() * blockSize); //setting ghost y in pixels
 
-	ghost.setSpeed((int) (Math.random() * Ghost.defaultSpeed) + 1); //ghost speed is randomly set upon release
-	
-	System.out.println("Spawning Point: " + releaseLocation.getX() + ", " + releaseLocation.getY());
+        ghost.setSpeed((int) (Math.random() * Ghost.defaultSpeed) + 1); //ghost speed is randomly set upon release
+
+        //System.out.println("Spawning Point: " + releaseLocation.getX() + ", " + releaseLocation.getY());
     }
     /**
      *Resets the timer on the ghost house
      *must wait a full time interval before the next ghost is released
      */
     public void resetTimer(){
-	lastReleaseTime = 0;
+        lastReleaseTime = 0;
     }
 }
