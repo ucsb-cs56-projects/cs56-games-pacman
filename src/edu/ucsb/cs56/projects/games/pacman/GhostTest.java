b62e7da328e3e70c4a25c76f70958e688e95f8a8
Wei Tung Chen <weitung_chen@umail.ucsb.edu>
2017-11-15 16:53:14 -0800

    Update indenting

--- src/edu/ucsb/cs56/projects/games/pacman/GhostTest.java
+++ src/edu/ucsb/cs56/projects/games/pacman/GhostTest.java
@@ -16,36 +16,46 @@ import static org.junit.Assert.assertEquals;
 public class GhostTest {
 	
 	@Test
-	public void test_constructor_simple() {
-		Ghost ghost = new Ghost(0, 0, 5, Ghost.GHOST1);
+	public void test_constructor_1() {
+		Ghost ghost = new Ghost(0, 0, 5, Ghost.TYPE_PINK);
 		assertEquals(0, ghost.x);
 		assertEquals(0, ghost.y);
 		assertEquals(5, ghost.speed);
-		assertEquals(Ghost.GHOST1, ghost.type);
+		assertEquals(Ghost.TYPE_PINK, ghost.type);
 	}
 	
 	@Test
-	public void test_constructor_complex() {
+	public void test_constructor_2() {
 		Ghost ghost = new Ghost(1, 2, 8, Ghost.GHOST2, new Grid());
-                assertEquals(1, ghost.x);
-                assertEquals(2, ghost.y);
-                assertEquals(8, ghost.speed);
-                assertEquals(Ghost.GHOST2, ghost.playerNum);
+		assertEquals(1, ghost.x);
+		assertEquals(2, ghost.y);
+		assertEquals(8, ghost.speed);
+		assertEquals(Ghost.GHOST2, ghost.playerNum);
 	}
 
 	@Test
-	public void test_death_oldStart() {
+	public void test_constructor_super() {
+		Ghost ghost = new Ghost(9, 8, 7, Ghost.TYPE_PINK, Ghost.GHOST2, new Grid());
+		assertEquals(9, ghost.x);
+		assertEquals(8, ghost.y);
+		assertEquals(7, ghost.speed);
+		assertEquals(Ghost.TYPE_PINK, ghost.type);
+		assertEquals(Ghost.GHOST2, ghost.playerNum);
+	}
+
+	@Test
+	public void test_death_1() {
 		Ghost ghost = new Ghost(3, 4, 5, Ghost.GHOST2);
 		ghost.death();
 		assertEquals(false, ghost.edible);
 	}
 
 	@Test
-        public void test_death_newStart() {
-                Ghost ghost = new Ghost(3, 4, 5, Ghost.GHOST2);
-                ghost.death(5, 8);
-                assertEquals(false, ghost.edible);
+	public void test_death_2() {
+		Ghost ghost = new Ghost(3, 4, 5, Ghost.GHOST2);
+		ghost.death(5, 8);
+		assertEquals(false, ghost.edible);
 		assertEquals(5, ghost.x);
 		assertEquals(8, ghost.y);
-        }
+	}
 }
